c7ab25f56d06e4ca0b57b77bd5f0a0e40c3c98d5
IgorKochetov <kochetovio@gmail.com>
2015-07-04 10:59:51 +0300

    Added credit card controller, initial action for inputing credit card number, made it default throughout the application

--- CashMachine/CashMachineWeb/App_Start/Startup.Auth.cs
+++ CashMachine/CashMachineWeb/App_Start/Startup.Auth.cs
@@ -14,7 +14,7 @@ namespace CashMachineWeb
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
+                LoginPath = new PathString("/CreditCardAccount/InputCardNumber")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
