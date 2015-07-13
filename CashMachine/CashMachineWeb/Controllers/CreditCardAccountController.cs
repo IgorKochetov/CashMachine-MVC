756f02a987ecbbf27640944bb18f9bf23d52112f
IgorKochetov <kochetovio@gmail.com>
2015-07-13 15:47:08 +0300

    First attempt at card input page and model

--- CashMachine/CashMachineWeb/Controllers/CreditCardAccountController.cs
+++ CashMachine/CashMachineWeb/Controllers/CreditCardAccountController.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using CashMachineWeb.Models;
 
 namespace CashMachineWeb.Controllers
 {
     public class CreditCardAccountController : Controller
     {
         //
-        // GET: /CreditCardAccount/
+        // GET: /CreditCardAccount/InputCardNumber
+        [AllowAnonymous]
         public ActionResult InputCardNumber()
         {
             return View();
         }
-	}
+
+        //
+        // POST: /CreditCardAccount/InputCardNumber
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> InputCardNumber(CreditCardNumber model)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+    }
 }
---
+++ CashMachine/CashMachineWeb/Models/CreditCardNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CashMachineWeb.Models
+{
+    public class CreditCardNumber
+    {
+        [Required]
+        [Display(Name = "Card number")]
+        [StringLength(maximumLength:4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+        public string PartOne { get; set; }
+
+        [Required]
+        [Display(Name = "Card number")]
+        [StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+        public string PartTwo { get; set; }
+
+        [Required]
+        [Display(Name = "Card number")]
+        [StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+        public string PartThree { get; set; }
+
+        [Required]
+        [Display(Name = "Card number")]
+        [StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+        public string PartFour { get; set; }
+
+    }
+}
