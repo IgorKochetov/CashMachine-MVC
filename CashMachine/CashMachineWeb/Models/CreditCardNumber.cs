a37e02ec21c86e10d637eb3ce56af5991b630de3
IgorKochetov <kochetovio@gmail.com>
2015-07-14 20:28:59 +0300

    Added binding to an actual model; fixed validation

--- CashMachine/CashMachineWeb/Models/CreditCardNumber.cs
+++ CashMachine/CashMachineWeb/Models/CreditCardNumber.cs
@@ -8,25 +8,34 @@ namespace CashMachineWeb.Models
 {
     public class CreditCardNumber
     {
-        [Required]
-        [Display(Name = "Card number")]
-        [StringLength(maximumLength:4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
-        public string PartOne { get; set; }
+		//[Required]
+		//[Display(Name = "Card number")]
+		//[StringLength(maximumLength:4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+		//public string PartOne { get; set; }
 
-        [Required]
-        [Display(Name = "Card number")]
-        [StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
-        public string PartTwo { get; set; }
+		//[Required]
+		//[Display(Name = "Card number")]
+		//[StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+		//public string PartTwo { get; set; }
 
-        [Required]
-        [Display(Name = "Card number")]
-        [StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
-        public string PartThree { get; set; }
+		//[Required]
+		//[Display(Name = "Card number")]
+		//[StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+		//public string PartThree { get; set; }
 
-        [Required]
-        [Display(Name = "Card number")]
-        [StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
-        public string PartFour { get; set; }
+		//[Required]
+		//[Display(Name = "Card number")]
+		//[StringLength(maximumLength: 4, ErrorMessage = "Should contain 4 digits", MinimumLength = 4)]
+		//public string PartFour { get; set; }
 
+		[Required]
+		[Display(Name = "Card number")]
+		[StringLength(maximumLength: 16, ErrorMessage = "Should contain 16 digits", MinimumLength = 16)]
+		public string ActualNumber { get; set; }
+
+		[Required]
+		[Display(Name = "Card number")]
+		[StringLength(maximumLength: 19, ErrorMessage = "Should contain 16 digits", MinimumLength = 19)]
+		public string DisplayNumber { get; set; }
     }
 }
