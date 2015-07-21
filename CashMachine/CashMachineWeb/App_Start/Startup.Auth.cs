db92511b5261e97c355ae7ee6eeba98b2580d2d4
IgorKochetov <kochetovio@gmail.com>
2015-07-21 11:55:20 +0300

    Removed 3rd party external cookie auth

--- CashMachine/CashMachineWeb/App_Start/Startup.Auth.cs
+++ CashMachine/CashMachineWeb/App_Start/Startup.Auth.cs
@@ -16,8 +16,8 @@ namespace CashMachineWeb
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/CreditCardAccount/InputCardNumber")
             });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+			//// Use a cookie to temporarily store information about a user logging in with a third party login provider
+			//app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
