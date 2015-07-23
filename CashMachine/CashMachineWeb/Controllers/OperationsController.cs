d3dc552bf9f4f84488cc789792b9e515660cd588
IgorKochetov <kochetovio@gmail.com>
2015-07-23 17:26:01 +0300

    Added hardcode redirect to error page from withdraw in order to test it. Added Back button to Error page

--- CashMachine/CashMachineWeb/Controllers/OperationsController.cs
+++ CashMachine/CashMachineWeb/Controllers/OperationsController.cs
@@ -31,5 +31,30 @@ namespace CashMachineWeb.Controllers
 		{
 			return View();
 		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Withdraw(object model)
+		{
+			if (ModelState.IsValid)
+			{
+				//var account = await accountManager.FindByNameAsync(model.ActualNumber);
+				//if (account != null)
+				//{
+				//	TempData["CreditCardNumber"] = model;
+				//	return RedirectToAction("InputPinNumber");
+				//}
+				//else
+				//{
+				//	ModelState.AddModelError("", "Card number is not found");
+				//}
+				TempData["ErrorMessage"] =
+					"Your request could not be proceed. You've exceeded your account limit on that operation request";
+				return RedirectToAction("Index", "Error");
+			}
+
+			// If we got this far, something failed, redisplay form
+			return View(model);
+		}
 	}
 }
