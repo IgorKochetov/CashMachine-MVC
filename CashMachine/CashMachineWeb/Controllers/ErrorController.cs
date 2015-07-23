fa677f83bb93121bdb2821059821879b688ebd58
IgorKochetov <kochetovio@gmail.com>
2015-07-23 16:30:02 +0300

    Error page added

---
+++ CashMachine/CashMachineWeb/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace CashMachineWeb.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+        public ActionResult Index()
+        {
+	        var errorMessage = TempData["ErrorMessage"];
+	        ViewBag.ErrorMessage = errorMessage;
+	        return View();
+        }
+	}
+}
